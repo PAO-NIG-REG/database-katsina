fd63ae1b93dcf87ee8d0e7f2f0f3935504a7f8c6
Busari Sheriff <busari.oluwagbemiga@gmail.com>
2015-08-13 13:55:02 +0100

    Added 3_reference_data.sql to the extension folders

---
+++ extension/3_reference_data.sql
@@ -0,0 +1,47 @@
+
+INSERT INTO application.request_type (code, request_category_code, display_value, description, status, nr_days_to_complete, base_fee, area_base_fee, value_base_fee, nr_properties_required, notation_template, rrr_type_code, type_action_code, service_panel_code, display_group_code, display_order) VALUES ('subdivideProperty', 'registrationServices', 'SubDivision of Property (General)::::???????? ????? (?????)::::????? ?? (???)::::Varier Droit (Général)::::::::Variar Direitos (Geral)::::???? (??)', '...::::...::::...::::...::::::::...::::...', 'c', 5, 5.00, 0.00, 0.00, 1, 'Variation of <right> <reference>', NULL, 'vary', 'property', 'generalReg', 180);
+
+
+INSERT INTO application.request_type_requires_source_type (source_type_code, request_type_code) VALUES ('deed', 'subdivideProperty');
+
+
+UPDATE application.request_type
+SET		base_fee= '2000',
+		area_base_fee= '0',
+		value_base_fee='0'
+WHERE code = 'newFreehold';
+
+UPDATE application.request_type
+SET		base_fee= '3000',
+		area_base_fee='0',
+		value_base_fee='0'
+WHERE code = 'titleSearch';
+
+UPDATE application.request_type
+SET		base_fee= '0',
+		area_base_fee='0',
+		value_base_fee='0.045'
+WHERE code = 'mortgage';
+
+UPDATE application.request_type
+SET		base_fee= '0',
+		area_base_fee='0',
+		value_base_fee='0.06'
+WHERE code = 'newOwnership';
+
+UPDATE application.request_type
+SET		base_fee= '20000',
+		area_base_fee='0',
+		value_base_fee='0'
+WHERE code = 'varyRight';
+
+UPDATE application.request_type
+SET		base_fee= '20000',
+		area_base_fee='0',
+		value_base_fee='0'
+WHERE code = 'subdivideProperty';
+
+
+
+
+
