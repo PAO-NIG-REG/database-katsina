6a2391d8a8def416d7f2d4a275c3d4c617d87680
unknown <RizzoM@NRC016.hq.un.fao.org>
2016-07-18 19:49:02 +0200

    changes from neil various emails

---
+++ changeset/1607c_89.sql
@@ -0,0 +1,5 @@
+﻿UPDATE application.request_type
+   SET  notation_template= 'Statutory Right of Occupancy'
+ WHERE code = 'newFreehold';
+
+INSERT INTO system.version SELECT '1607c' WHERE NOT EXISTS (SELECT version_num FROM system.version WHERE version_num = '1607c');
